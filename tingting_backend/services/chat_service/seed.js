efe0d3c298b2e86f17b4ab0b6ceab7148d3ded56
Thùy Nhi IT <21054781.nhi@student.iuh.edu.vn>
2025-04-09 12:58:08 +0700

    update seed.js

--- tingting_backend/services/chat_service/seed.js
+++ tingting_backend/services/chat_service/seed.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-const Conversation = require('./models/Conversation'); 
-const Message = require('./models/Message'); 
+const Conversation = require('./models/Conversation');
+const Message = require('./models/Message');
 dotenv.config();
 
 const conversations = [
@@ -12,12 +12,13 @@ const conversations = [
         linkGroup: "https://groupchat.com/tech",
         imageGroup: "https://imageurl.com/techgroup.png",
         participants: [
-            { userId: "6601a1b2c3d4e5f678901238", role: "admin", mute: "1h" },
-            { userId: "6601a1b2c3d4e5f678901239", role: "member", mute: null },
+            {
+                userId: "6601a1b2c3d4e5f678901238", role: "admin", mute: "1h", isHidden: false, isPinned: false
+            },
+            { userId: "6601a1b2c3d4e5f678901239", role: "member", mute: null, isHidden: false, isPinned: false },
         ],
         lastMessage: null,
-        isHidden: false,
-        isPinned: false,
+
         createAt: new Date(),
         updateAt: new Date()
     },
@@ -26,8 +27,8 @@ const conversations = [
         name: "Chat Cá Nhân",
         isGroup: false,
         participants: [
-            { userId: "6601a1b2c3d4e5f678901240", mute: null },
-            { userId: "6601a1b2c3d4e5f678901241" , mute: null },
+            { userId: "6601a1b2c3d4e5f678901240", mute: null ,isHidden: false, isPinned: false},
+            { userId: "6601a1b2c3d4e5f678901241", mute: null, isHidden: false, isPinned: false },
         ],
         lastMessage: null,
         createAt: new Date(),
@@ -91,7 +92,7 @@ const messages = [
         deletedBy: [],
         createdAt: new Date()
     },
-    
+
     // Tin nhắn cho cuộc trò chuyện cá nhân
     {
         conversationId: conversations[1]._id,
@@ -139,13 +140,13 @@ mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
-.then(async () => {
-    console.log("Kết nối MongoDB thành công!");
-    await Conversation.deleteMany();
-    await Message.deleteMany();
-    await Conversation.insertMany(conversations);
-    await Message.insertMany(messages);
-    console.log("Dữ liệu đã được thêm thành công!");
-    mongoose.connection.close();
-})
-.catch(err => console.error("Lỗi kết nối MongoDB:", err));
+    .then(async () => {
+        console.log("Kết nối MongoDB thành công!");
+        await Conversation.deleteMany();
+        await Message.deleteMany();
+        await Conversation.insertMany(conversations);
+        await Message.insertMany(messages);
+        console.log("Dữ liệu đã được thêm thành công!");
+        mongoose.connection.close();
+    })
+    .catch(err => console.error("Lỗi kết nối MongoDB:", err));
