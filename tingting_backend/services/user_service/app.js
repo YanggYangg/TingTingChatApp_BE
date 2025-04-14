82d62902690b06cfb966dfbc2e0da489c6d6f298
21034451_NguyenThiQuynhGiang <nguyenquynhgiang1324@gmail.com>
2025-04-14 11:55:48 +0700

    Chang type=sms&prioritize=1

--- tingting_backend/services/user_service/app.js
+++ tingting_backend/services/user_service/app.js
@@ -11,7 +11,6 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(cookieParser());
 app.use(cors({
     origin: "http://localhost:5173",
     credentials: true
