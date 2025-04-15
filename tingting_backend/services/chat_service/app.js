274f662d63fe549b9f6e442757c89a4b97c8ac82
21034451_NguyenThiQuynhGiang <nguyenquynhgiang1324@gmail.com>
2025-04-15 15:54:07 +0700

    Fix merge

--- tingting_backend/services/chat_service/app.js
+++ tingting_backend/services/chat_service/app.js
@@ -15,7 +15,7 @@ const server = http.createServer(app);
 initializeSocket(server);
 
 app.use(cors({
-    origin: '*',
+    origin: 'http://localhost:5173',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept', 'Origin'],
     credentials: true
