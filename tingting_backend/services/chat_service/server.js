9b46a482f8f5cbfb8a482546e1b1193fd5be837a
hominhhau <hauxoan333@gmail.com>
2025-04-24 01:52:57 +0700

    ver1

--- tingting_backend/services/chat_service/server.js
+++ tingting_backend/services/chat_service/server.js
@@ -1,6 +1,6 @@
 const { app, server } = require('./app');
 const PORT = process.env.PORT || 5000;
 
-server.listen(PORT, () => {
+server.listen(PORT, '0.0.0.0', () => {
     console.log(`Server/service running on port ${PORT} .....`);
 });
