37baa242fd148626e1d9161d41ccabb9d6e4c1f6
21034451_NguyenThiQuynhGiang <nguyenquynhgiang1324@gmail.com>
2025-05-19 01:09:02 +0700

    Done BE 19.05

--- tingting_backend/services/chat_service/services/socket/handlers/readStatus.js
+++ tingting_backend/services/chat_service/services/socket/handlers/readStatus.js
@@ -24,6 +24,7 @@ module.exports = {
                     userId,
                     readBy: message.status.readBy,
                 });
+
                 logger.info(`User ${userId} read message ${messageId}`);
             }
         } catch (error) {
